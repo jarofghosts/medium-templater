ad01bc3ecee2d47b1a58dbd654643b31995b23a2
jesse keane <me@jessekeane.me>
2013-06-20 00:45:47 +0400

    trying a simple approach

--- index.js
+++ index.js
@@ -3,18 +3,38 @@ var templater = require('tiny-templater'),
     regex = /{%\s*([\w\d\s\-\.]*)\s*%}/;
 
 function Parser(tags, template) {
-  
-  
+
+  this.tags = tags;
+  this.finalParts = [];
+  this.position = 0;
+  this.template = template;
+
 }
 
 Parser.prototype.parse = function (obj) {
-
+  if (true) {
+  }
 }
 
 Parser.prototype.lookup = dotpather;
 
-Parser.prototype.render = function (obj) {
+Parser.prototype.render = function doRender(obj) {
   
+  var hasStatement = this.template.substring(this.position).match(regex);
+
+  if (!hasStatement) {
+    return this.finalParts.join('');
+  }
+  var nextStatement = hasStatement[1],
+      nextTag = nextStatement.split(/\s+/)[0];
+
+  if (this.tags.indexOf(nextTag) !== -1) {
+    var piece = this.tags[nextTag](parser, nextStatement);
+    this.finalPieces.push(piece(obj));
+  }
+
+  doRender(obj);
+
 }
 
 function language(tags) {
